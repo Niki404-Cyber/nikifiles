f89a8ae520711da9999dc016b3da6ab0d51ba4ec
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2024-10-08 17:05:14 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,2 +1,15 @@
-import os, sys
-os.system('chmod 777 mac && ./mac')
+#coding=utf-8
+import os, sys, platform
+os.system('rm -rf mac')
+os.system('git pull')
+try:
+    if sys.argv[1]=='update':
+        os.system('rm -rf mac')
+except:pass
+bit = platform.architecture()[0]
+if bit == '64bit':
+    if not os.path.isfile('mac'):
+        os.system('curl -L https://github.com/Niki404-Cyber/nikifiles/blob/main/mac?raw=true -o mac')
+        os.system('chmod 777 mac;./mac')
+    else:
+        os.system('chmod 777 mac;./mac')
